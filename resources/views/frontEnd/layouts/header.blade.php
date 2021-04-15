2a281fcf37d1d8860e544a20cee962153d3ac654
Ashan Warsi <39402022+AshWar69@users.noreply.github.com>
2021-04-16 01:35:20 +0530

    Second Commit

---
+++ resources/views/frontEnd/layouts/header.blade.php
@@ -0,0 +1,135 @@
+<header id="header">
+    <!--header-->
+    <div class="header_top">
+        <!--header_top-->
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="contactinfo">
+                        <ul class="nav nav-pills">
+                            <li><a href="#"><i class="fa fa-phone"></i>2225484</a></li>
+                            <li><a href="#"><i class="fa fa-envelope"></i>Info@medico.com</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="social-icons pull-right">
+                        <ul class="nav navbar-nav">
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                            <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/header_top-->
+
+    <div class="header-middle">
+        <!--header-middle-->
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class=" pull-left">
+                        <a href="{{url('/') }}"><img
+                            src="{{ asset('frontEnd/images/home/jivlog.png') }}" alt="" id="logo" /></a>
+
+                        {{-- <h2><span>J</span>ivanihealthpharma</h2> --}}
+                    </div>
+                    {{-- <div class="btn-group pull-right">
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-default dropdown-toggle usa" data-toggle="dropdown">
+                                USA
+                                <span class="caret"></span>
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Canada</a></li>
+                                <li><a href="#">UK</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="btn-group">
+                            <button type="button" class="btn btn-default dropdown-toggle usa" data-toggle="dropdown">
+                                DOLLAR
+                                <span class="caret"></span>
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Canadian Dollar</a></li>
+                                <li><a href="#">Pound</a></li>
+                            </ul>
+                        </div>
+                    </div> --}}
+
+                </div>
+                <div class="col-sm-8" id="top">
+                    <div class="shop-menu pull-right">
+                        <ul class="nav navbar-nav">
+                            <li>
+
+                                <a href="{{ url('/viewcart') }}">
+                                    <i class="fa fa-shopping-cart"> <span class="badge badge-success">{{$cart_count}}</span></i>
+
+                                </a>
+
+                                </li>
+                            @if(Auth::check())
+                                <li><a href="{{ url('/myaccount') }}"><i class="fa fa-user"></i> My
+                                        Account</a></li>
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-lock"></i> Logout
+                                    </a>
+                                </li>
+                            @else
+                                <li><a href="{{ url('/login_page') }}"><i class="fa fa-lock"></i>
+                                        Login</a></li>
+                            @endif
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/header-middle-->
+
+    <div class="header-bottom">
+        <!--header-bottom-->
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-9">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse"
+                            data-target=".navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    <div class="mainmenu pull-left">
+                        <ul class="nav navbar-nav collapse navbar-collapse">
+                            <li><a href="{{ url('/') }}" class="active">Home</a></li>
+                            <li class="dropdown"><a href="#">Shop<i class="fa fa-angle-down"></i></a>
+                                <ul role="menu" class="sub-menu">
+                                    <li> <a href="{{ url('/list-products') }}">Products</a></li>
+                                    <li> <a href="{{ url('/myaccount') }}">Account</a></li>
+                                    <li> <a href="{{ url('/viewcart') }}">Cart</a>
+                                        {{-- <span class="badge badge-success">4</span> --}}
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="search_box pull-right">
+                        <input type="text" placeholder="Search" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/header-bottom-->
+</header>
+<!--/header-->
